5bbd8eabd23c5d2d83a5119bd3e9a8ce23521011
Malcolm Sng <malcolmsng99@gmail.com>
2022-02-26 15:56:13 +0800

    first commit

---
+++ application_frontend/lib/utils/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+//colors
+const Color magenta = Color(0xFFEF476F);
+const Color offWhite = Color(0xFFedf2f4);
+const Color yellow = Color(0xFFffd166);
+const Color green = Color(0xFF06d6a0);
+const Color blue = Color(0xFF118ab2);
+const Color navyBlue = Color(0xFF073b4c);
+
+//fonts
+
+
+//fontWeights
+const FontWeight kregular = FontWeight.w400;
+const FontWeight kmedium = FontWeight.w500;
+const FontWeight ksemiBold = FontWeight.w600;
+const FontWeight kbold = FontWeight.w700;
