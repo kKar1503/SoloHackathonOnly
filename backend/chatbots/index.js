04590e288eefc285292d65603508278d342894a5
kKar1503 <isonice4572@gmail.com>
2022-02-26 16:38:47 +0800

    added echo reply as a base

---
+++ backend/chatbots/index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+const { TELEGRAM_TOKEN, SERVER_URL } = process.env;
+const TELEGRAM_API = `https://api.telegram.org/bot${TELEGRAM_TOKEN}`;
+const URI = `/webhook/${TELEGRAM_TOKEN}`;
+const WEBHOOK_URL = SERVER_URL + URI;
+
+const TelegramServer = require('./telegram/index');
+
+const InitializeTelegramBot = () => {
+	if (!TELEGRAM_TOKEN) {
+		return new Error('Missing Token: Please specify the Telegram bot token in the TOKEN Environmental Variable.');
+	}
+
+	return new TelegramServer(PORT, TELEGRAM_API, WEBHOOK_URL, URI);
+};
+
+module.exports = { telegram: InitializeTelegramBot };
--- backend/chatbots/telegram/index.js
+++ backend/chatbots/telegram/index.js
@@ -1,14 +1,28 @@
 const express = require('express');
-const axios = require('axios');
+const axios = require('axios').default;
 const bodyParser = require('body-parser');
 
 class TelegramServer {
-	constructor(port, api, webhook) {
+	constructor(port, api, webhook, uri) {
 		this.app = express();
 
 		this.app.use(bodyParser.json());
 
-		listen(port, async () => {
+		this.app.post(uri, async (req, res) => {
+			console.log(req.body);
+
+			const chatId = req.body.message.chat.id;
+			const text = req.body.message.text;
+
+			await axios.post(`${api}/sendMessage`, {
+				chat_id: chatId,
+				text: text,
+			});
+
+			return res.send();
+		});
+
+		this.app.listen(port, async () => {
 			console.log(`App running on port ${port}`);
 			await this.init(api, webhook);
 		});
