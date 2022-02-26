80ce7cb99d8d3324502f20d952a58041d76d7432
kKar1503 <isonice4572@gmail.com>
2022-02-27 02:13:18 +0800

    Added firestore and validation of users with firestore info

--- backend/chatbots/telegram/index.js
+++ backend/chatbots/telegram/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const axios = require('axios').default;
 const bodyParser = require('body-parser');
-const validation = require('./validation');
+const { validation } = require('./validation');
+const jwt = require('jsonwebtoken');
+const { async } = require('@firebase/util');
 
 class TelegramServer {
 	constructor(port, api, webhook, uri) {
@@ -10,10 +12,19 @@ class TelegramServer {
 		this.app.use(bodyParser.json());
 
 		this.app.post(uri, async (req, res) => {
-			console.log(req.body);
+			const ctx = req.body;
+			const chat_id = ctx.message.chat.id;
+			const message = ctx.message.text;
 
-			if (!validation.validate(req.body)) {
+			if (!(await validation.validate(ctx))) {
+				await this.sendMessage(
+					`You do not have any account linked with Doctor.io, please register a Doctor.io account and link your Telegram account.`,
+					api,
+					chat_id
+				);
 			}
+
+			await this.sendMessage(`Your user type is: ${await validation.userType(ctx)}`, api, chat_id);
 
 			return res.send();
 		});
@@ -27,6 +38,13 @@ class TelegramServer {
 	init = async (api, webhook) => {
 		const res = await axios.get(`${api}/setWebhook?url=${webhook}`);
 		console.log(res.data);
+	};
+
+	sendMessage = async (message, api, chat_id) => {
+		await axios.post(`${api}/sendMessage`, {
+			chat_id: chat_id,
+			text: message,
+		});
 	};
 }
 
--- backend/chatbots/telegram/validation.js
+++ backend/chatbots/telegram/validation.js
@@ -1,7 +1,22 @@
-// const
+const { async } = require('@firebase/util');
+const { patients, doctors } = require('./firestore');
 
 let validation = {
-	validate: (ctx) => {
-        if (ctx.message.from.id)
-    },
+	validate: async (ctx) => {
+		const patient = await patients.where('telegram.id', '==', ctx.message.from.id).get();
+
+		return patient.docs.length != 0;
+	},
+	userType: async (ctx) => {
+		const patient = await patients.where('telegram.id', '==', ctx.message.from.id).get();
+		const doctor = await doctors.where('telegram.id', '==', ctx.message.from.id).get();
+		if (patient.docs.length != 0) {
+			return 'Patient';
+		}
+		if (doctor.docs.length != 0) {
+			return 'Doctor';
+		}
+	},
 };
+
+module.exports = { validation };
